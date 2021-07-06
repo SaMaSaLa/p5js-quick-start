d7ab9b8d6631b02bb42cecae6ba141359433b9e0
Sai Vanga <Sai@Sai.Rocks>
2021-07-06 13:00:08 -0700

    Made Small Game

---
+++ sketch.js
@@ -0,0 +1,20 @@
+noStroke();
+
+function setup() {
+    createCanvas(1500, 675);
+    background(200, 200, 200);
+}
+
+function draw() {
+    if (mouseIsPressed) {
+        fill(0);
+    } else {
+        fill(255);
+    }
+    fill(0, 0, 0);
+    ellipse(mouseX, mouseY, 80, 80);
+}
+
+function mouseClicked() {
+    background(200, 200, 200);
+}
